e4ca4be9f8d9e075cffd3ad1b36f7b8c86d519ff
Oris John-Baptiste <orisjb@gmail.com>
2024-10-11 11:55:48 +0100

    broken validation

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -29,6 +29,7 @@ export default function EditInvoiceForm({
           </label>
           <div className="relative">
             <select
+              required
               id="customer"
               name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
@@ -62,6 +63,8 @@ export default function EditInvoiceForm({
                 defaultValue={invoice.amount}
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                required
+                aria-describedby="amount-error"
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
@@ -83,6 +86,8 @@ export default function EditInvoiceForm({
                   value="pending"
                   defaultChecked={invoice.status === "pending"}
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  required
+                  aria-describedby="pending-error"
                 />
                 <label
                   htmlFor="pending"
@@ -99,6 +104,8 @@ export default function EditInvoiceForm({
                   value="paid"
                   defaultChecked={invoice.status === "paid"}
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  required
+                  aria-describedby="paid-error"
                 />
                 <label
                   htmlFor="paid"
